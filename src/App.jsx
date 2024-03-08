3d07fd58328dedf4e599e7ea4050d5866d36c9c1
Lautaro Montaña <joselautarom@gmail.com>
2024-03-07 22:00:03 -0300

    ReactJS - Clase 4

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { NavBar } from './components/NavBar/NavBar'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <h1>Hola Mundo!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+// import React from 'react';
+
+// const NavBar = () =>{
+//     return(
+//         <h1>Soy el menu</h1>
+//     )
+// }
+
+// export default NavBar
+
+import React from 'react'
+import './NavBar.css'
+
+export const NavBar = () => {
+    return (
+        <>
+            <nav>
+                <a href="">Inicio</a>
+                <a href="">Contacto</a>
+                <a href="">Nosotros</a>
+            </nav>
+        </>
+    )
+}
+
+
+
